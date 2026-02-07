32ac82b08b024c4e5e99901d919e462e0a5e8fc8
zhishi <1951671751@qq.com>
2026-02-07 23:32:47 +0800

    修复api删除，导致模型删除问题

--- src/routes/setting/getAiModelMap.ts
+++ src/routes/setting/getAiModelMap.ts
@@ -8,6 +8,6 @@ export default router.post("/", async (req, res) => {
   const configData = await u
     .db("t_aiModelMap")
     .leftJoin("t_config", "t_aiModelMap.configId", "t_config.id")
-    .select("t_aiModelMap.name", "t_config.model", "t_aiModelMap.id");
+    .select("t_aiModelMap.name", "t_config.model", "t_aiModelMap.id", "t_aiModelMap.key");
   res.status(200).send(success(configData));
 });
